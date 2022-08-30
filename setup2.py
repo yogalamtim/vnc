90c68e6314e63d1582643f58f291a526afbe79f6
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-08-30 20:09:57 +0700

    Create setup2.py

---
+++ setup2.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "sipo2.py",
+    version = "0.1",
+    py_modules = ['sipo2'],
+    url = "https://github.com/demotomohiro/sipo2",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
