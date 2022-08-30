89cecbaccd91b6f9873d55a0205c504809346110
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-08-30 10:07:19 +0700

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -234,9 +234,9 @@ def _setupSSHDImpl(public_key, tunnel, ngrok_token, ngrok_region, mount_gdrive_t
     msg += "✂️"*24 + "\n"
   return msg
 
-def _setupSSHDMain(public_key, tunnel, ngrok_region, check_gpu_available, mount_gdrive_to, mount_gdrive_from, is_VNC):
-  if check_gpu_available and not _check_gpu_available():
-    return (False, "")
+#def _setupSSHDMain(public_key, tunnel, ngrok_region, check_gpu_available, mount_gdrive_to, mount_gdrive_from, is_VNC):
+ # if check_gpu_available and not _check_gpu_available():
+  #  return (False, "")
 
   print("---")
   avail_tunnels = {"ngrok", "argotunnel"}
