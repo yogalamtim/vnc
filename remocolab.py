9e0a6535b689a5f35e122a6e9adf9c865836ac15
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-08-30 10:03:01 +0700

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -103,7 +103,7 @@ def _get_gpu_name():
 #  else:
 #    return True
 
-#  return IPython.utils.io.ask_yes_no("Do you want to continue? [y/n]")
+  return IPython.utils.io.ask_yes_no("Do you want to continue? [y/n]")
 
 def _set_public_key(user, public_key):
   if public_key != None:
