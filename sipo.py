63ee86a6166de15059f1b5c6847b63322051bf82
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-08-31 20:58:02 +0700

    Update sipo.py

--- sipo.py
+++ sipo.py
@@ -233,7 +233,7 @@ def _setupSSHDImpl(public_key, tunnel, ngrok_token, ngrok_region, mount_gdrive_t
     msg += "Command to connect to the ssh server:\n"
     msg += "✂️"*24 + "\n"
     #msg += f"ssh {ssh_common_options} {user_name}@{hostname}\n"
-    #msg += "✂️"*24 + "\n"
+    msg += "✂️"*24 + "\n"
   return msg
 
 def _setupSSHDMain(public_key, tunnel, ngrok_region, check_gpu_available, mount_gdrive_to, mount_gdrive_from, is_VNC):
